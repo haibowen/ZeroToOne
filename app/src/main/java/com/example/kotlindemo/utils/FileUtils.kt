8d7ab922f81a2021e45fb061e370caf541e6e926
wenhaibo <password>
2021-04-15 09:59:52 +0800

    文件提交操作

---
+++ app/src/main/java/com/example/kotlindemo/utils/FileUtils.kt
@@ -0,0 +1,23 @@
+package com.example.kotlindemo.utils
+
+/**
+ * 新建一个文件操作类
+ */
+
+class FileUtils {
+
+    /**
+     * 上传
+     */
+    fun upload(){
+
+    }
+
+    /**
+     * 下载
+     */
+    fun download(){
+
+    }
+
+}
