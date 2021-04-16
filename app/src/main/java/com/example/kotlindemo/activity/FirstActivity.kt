b0e176a562842ab250e2b163fadb6ae29c134ab6
wenhaibo <password>
2021-04-16 20:09:23 +0800

    提交横向滑动的view

--- app/src/main/java/com/example/kotlindemo/activity/FirstActivity.kt
+++ app/src/main/java/com/example/kotlindemo/activity/FirstActivity.kt
@@ -96,5 +96,10 @@ class FirstActivity : BaseActivity() {
             dialog.show()
 
         }
+        bt_home_13.setOnClickListener {
+
+            startActivity(Intent(this,RecyclerViewActivity::class.java))
+
+        }
     }
 }
