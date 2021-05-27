d19e3a89b23c78c9585fb5e3921c5d912a5577ae
hyplo <1461154748@qq.com>
2021-05-27 22:33:36 +0800

    提交

---
+++ gradle-test/src/main/java/com/example/gradle_test/HelloGroovy.java
@@ -0,0 +1,24 @@
+package com.example.gradle_test;
+
+/**
+ * Created on 5/24/21
+ * Author: hyplo
+ * Email: haibowen088@gmail.com
+ * Description: show me the code change the world
+ */
+public class HelloGroovy {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+
+            System.out.println("ha");
+        }
+        System.out.println("hello Groovy");
+    }
+
+    void test(){
+
+        
+    }
+}
+
+
