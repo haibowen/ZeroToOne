40c717895d15e2bd002a1df2d0eb2bae7e59f9bf
hyplo <1461154748@qq.com>
2021-08-07 10:09:50 +0800

    提交

---
+++ jetpack/build/generated/source/buildConfig/debug/com/example/jetpack/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.jetpack;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.example.jetpack";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ jetpack/build/generated/source/buildConfig/release/com/example/jetpack/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.jetpack;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.example.jetpack";
+  public static final String BUILD_TYPE = "release";
+}
